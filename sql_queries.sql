5f431e9bad811d72a30da9c1fd2fc1e56373ebd0
jgandre <67715755+jgandre@users.noreply.github.com>
2020-10-20 02:36:15 -0500

    Add files via upload
    First 5 queries

---
+++ sql_queries.sql
@@ -0,0 +1,56 @@
+-- List the following details of each employee:
+-- employee number, last name, first name, sex, and salary.
+
+SELECT e.emp_no, e.first_name, e.last_name, e.sex, s.salary
+FROM employees e
+JOIN salaries s
+ON e.emp_no = s.emp_no;
+
+--List first name, last name, and hire date for employees who were hired in 1986.
+SELECT e.emp_no, e.first_name, e.last_name, e.hire_date
+FROM employees e
+WHERE e.hire_date >= '1986-01-01' 
+AND e.hire_date <= '1986-12-31';
+
+-- List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+-- join the depts to dept_mgr to  emp
+SELECT d.dept_no , d.dept_name , dm.emp_no, e.first_name, e.last_name
+FROM departments d
+JOIN dept_manager dm
+ON d.dept_no = dm.dept_no
+JOIN employees e
+ON dm.emp_no = e.emp_no;
+
+-- List the department of each employee with the following information: employee number, last name, first name, and department name.
+-- emp to dep_emp to dept
+
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+JOIN dep_employees de
+ON e.emp_no = de.emp_no
+JOIN departments d
+ON de.dept_no = d.dept_no;
+
+
+-- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT e.first_name, e.last_name, e.sex
+FROM employees e
+WHERE e.first_name = 'Hercules'
+AND e.last_name LIKE 'B%';
+
+
+-- List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+JOIN dep_employees de
+ON e.emp_no = de.emp_no
+JOIN departments d
+ON de.dept_no = d.dept_no;
+
+
+
+
+
+
+-- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+-- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
